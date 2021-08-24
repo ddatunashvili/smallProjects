c66815e3e2888b1586f6cc553fa9ccf1ece43778
ddatunashvili <davitidatunashvili98@gmail.com>
2021-08-25 01:32:17 +0400

    სიას დაემატა გადასვლის პანელი

---
+++ panel_transition/script.js
@@ -0,0 +1,20 @@
+
+// all in node list array
+const panels = document.querySelectorAll(".panel")
+
+
+panels.forEach((panel)=>{
+    panel.addEventListener('click', () => {
+        removeActive()
+        panel.classList.add("active")
+    })
+})
+
+function removeActive(){
+    panels.forEach(panel => {
+        panel.classList.remove("active")
+    })
+}
+
+
+
